2f44c2529439ef497289ceda8894261d704d1035
osaechan <dhtocks@naver.com>
2021-06-06 17:41:54 +0900

    fix intent page

--- dialobot/app/server.py
+++ dialobot/app/server.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
